5417f8771a09c22fe6d990739f872329c145cdb5
mikeC409 <mike21cahow@gmail.com>
2024-10-19 11:28:37 -0500

    Updated .gitignore and program now opens up a black screen and closes without error

--- main.py
+++ main.py
@@ -3,7 +3,30 @@ import pygame
 import constants
 
 def main():
+    # initialize pygame
     pygame.init()
+
+    # set screen width and heigh from our constants
+    screen_width = constants.SCREEN_WIDTH
+    screen_height = constants.SCREEN_HEIGHT
+
+    # set up the display
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption("Asteroids")
+
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        # set display black
+        screen.fill((0, 0, 0))
+
+        # refresh screen
+        pygame.display.flip()
+                
+
 
     print("Starting asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
